9654c5c229b0b3d72eadc688453c8dffeb75c67d
FelipeGarLima <meca_felipe@hotmail.com>
2023-06-26 13:25:06 -0300

    Esse está com autenticação bcrypt

--- authService.js
+++ authService.js
@@ -1,22 +1,25 @@
 // autenticacao/authService.js
+const bcrypt = require('bcrypt');
 
-function autenticarUsuario(usuario, senha) {
+//function autenticarUsuario(usuario, senha) {
+async function autenticarUsuario(usuario, senha) {
   // Simulando um usuário fixo
   const user = {
     username: 'admin',
-    //password: '$2b$10$WmM0koCJrDqLftQnR7XO4usDhTj8KsFn6XYIcLnkkh3F5mbqP9T3K' // senha: 'admin'
-    password: 'admin'
+    password: '$2b$10$WmM0koCJrDqLftQnR7XO4usDhTj8KsFn6XYIcLnkkh3F5mbqP9T3K' // senha: 'admin'
+    //password: 'admin'
   };
+  const autenticacao = await bcrypt.compareSync(senha, user.password);
+  console.log(autenticacao);
+  if (usuario === user.username && autenticacao === true) {
+    //if (usuario === user.username && senha === user.password) {
 
-    //if (username === user.username && bcrypt.compareSync(password, user.password)) {  
-    if (usuario === user.username && senha === user.password) {
-      
-      return true;
-    } else {
-   
-      return false;
-    }
-  
+    return true;
+  } else {
+
+    return false;
+  }
+
 }
 
 module.exports = {
